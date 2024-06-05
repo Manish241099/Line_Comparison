63c826195dae6a2e8b28454e0ef61dc328f8b77f
Manish241099 <manishkhade241099@gmail.com>
2024-06-05 23:32:48 +0530

    UC1

---
+++ src/LineComparison.java
@@ -0,0 +1,18 @@
+
+    public class LineComparison {
+        public void isEqual(double x1,double x2,double y1,double y2){
+
+
+            double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+            System.out.println("Length of line is :"+length_line);
+        }
+
+        public static void main(String[] args) {
+
+            LineComparison uc1 = new LineComparison();
+            uc1.isEqual(1, 5, 4, 9);
+
+
+        }
+    }
+
